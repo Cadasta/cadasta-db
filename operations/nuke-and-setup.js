5ed173a9cb55dcdddc6aa51725043e810654edab
Daniel Baah <dbaah@spatialdev.com>
2015-10-06 13:26:46 -0700

    all views show ckan_id automated node process to nuke and re-build db

--- operations/nuke-and-setup.js
+++ operations/nuke-and-setup.js
@@ -1,11 +1,168 @@
+var pg = require('pg');
+var Q = require('q');
+var fs = require('fs');
+var exec = require('child_process').exec;
+
+// Get DB settings for the requested environment
+var settings = require('../settings.js').local;
+
+// build DB connection string
+var conString = "postgres://" +
+    settings.user + ":" +
+    settings.password + "@" +
+    settings.server + ":" +
+    settings.port + "/" +
+    settings.database;
+
 /**
- * This operation destroys the database in the settings file
- * and builds a new one ready for data ingestion.
- *
- * Note: if you are having problems running this, try
- * closing pgAdmin3.
+ * This destroys the current database and recreates a
+ * fresh one with no tables or functions added yet...
  */
+function nukeDB() {
 
-var pgSetup = require('../src/pg-setup.js');
+    var deferred = Q.defer();
 
-pgSetup.nukeDB(pgSetup.setupDB);
+    var db = settings.database;
+    var kill = 'psql -d ' + db + ' -U postgres -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = \'' + db
+        + '\' AND pid <> pg_backend_pid()";';
+    var drop = 'dropdb ' + db;
+    var create = 'createdb ' + db + ' -O ' + settings.user;
+
+    exec(kill, function(error, stdout, stderr) {
+
+        exec(drop, function(error, stdout, stderr) {
+            if (stderr) console.error(stderr);
+
+            exec(create, function (error, stdout, stderr) {
+                if (stderr) {
+                    console.error(stderr);
+                    return deferred.reject(stderr);
+                }
+
+                else {
+                    console.log('db nuked and recreated.');
+                    return deferred.resolve(true)
+                }
+
+            });
+        });
+    });
+
+    return deferred.promise;
+}
+
+
+function runDeferredPsql(command){
+
+    var deferred = Q.defer();
+
+    exec(command, function (error, stdout, stderr) {
+        if (stderr) {
+            console.error(stderr);
+            return deferred.reject(stderr);
+        }
+
+        else {
+            console.log('success.');
+            return deferred.resolve(true)
+        }
+
+    });
+
+    return deferred.promise;
+
+}
+/**
+ * Query function to execute an SQL query against Postgres DB.
+ *
+ * @type {Function}
+ */
+function query(queryStr) {
+
+    var deferred = Q.defer();
+
+    pg.connect(conString, function(err, client, done) {
+        if(err) {
+            console.error('error fetching client from pool', err);
+        }
+        client.query(queryStr, function(queryerr, result) {
+
+            done();
+
+            if(queryerr) {
+                //console.error('ERROR RUNNING QUERY:', queryStr, queryerr);
+                deferred.reject(queryerr);
+            } else {
+                deferred.resolve(result && result.rows ? result.rows : result);
+            }
+
+        });
+    });
+
+    return deferred.promise;
+}
+
+/*
+ var q1 = fs.readFileSync('node_modules/cadasta-db/sql/1_db.sql', 'utf8');
+ var q2 = fs.readFileSync('node_modules/cadasta-db/sql/2_field-data-tables.sql');
+ var q3 = fs.readFileSync('node_modules/cadasta-db/sql/3_db-functions.sql');
+ var q4 = fs.readFileSync('node_modules/cadasta-db/sql/4_db-views.sql');
+ var q5 = fs.readFileSync('node_modules/cadasta-db/sql/5_validation-functions.sql');
+ var q6 = fs.readFileSync(__dirname + '/truncate-db-tables.sql', 'utf8');
+ */
+return nukeDB()
+
+    .then(function(response){
+        return runDeferredPsql('psql -U postgres -d ' + settings.database + ' -q -f ./sql/1_db.sql');
+
+    })
+    .then(function(){
+
+        console.log('1_db.sql complete.');
+        return runDeferredPsql('psql -U postgres -d ' + settings.database + ' -q -f ./sql/2_field-data-tables.sql');
+
+    })
+
+
+    .then(function(){
+
+        console.log('2_survey-tables.sql complete.');
+        return runDeferredPsql('psql -U postgres -d ' + settings.database + ' -q -f ./sql/3_db-functions.sql');
+
+
+    })
+    .then(function(){
+
+        console.log('3_db-functions.sql complete.');
+        return runDeferredPsql('psql -U postgres -d ' + settings.database + ' -q -f ./sql/4_db-views.sql');
+
+    })
+    .then(function(){
+        console.log("4_db-view is complete.")
+        return runDeferredPsql('psql -U postgres -d ' + settings.database + ' -q -f ./sql/5_validation-functions.sql');
+
+    })
+    .then(function(){
+        console.log("5_validation-functions.sql is complete.")
+        return runDeferredPsql('psql -U postgres -d ' + settings.database + ' -q -f ./sql/6_test-data.sql');
+
+    })
+
+    .then(function(){
+
+        console.log('6_test-data.sql is complete.');
+        return runDeferredPsql('psql -U postgres -d ' + settings.database + ' -q -f ./sql/truncate-db-tables.sql');
+
+
+    })
+    .then(function(){
+
+        console.log('truncate function is complete.');
+        return ;
+
+
+    })
+    .catch(function(err){
+        console.log(err);
+    })
+    .done();
--- sql/4_db-views.sql
+++ sql/4_db-views.sql
@@ -8,7 +8,7 @@
 
 -- Show all relationships
 CREATE OR replace view show_relationships AS
-SELECT r.id AS relationship_id, t.type AS relationship_type, parcel.id AS parcel_id, project.id AS project_id, s.type AS spatial_source, project.title as project_title, COALESCE(rg.geom,parcel.geom) as geom,
+SELECT r.id AS relationship_id, t.type AS relationship_type, parcel.id AS parcel_id, project.id AS project_id, project.ckan_id, s.type AS spatial_source, project.title as project_title, COALESCE(rg.geom,parcel.geom) as geom,
 party.id AS party_id, first_name, lASt_name, r.time_created,r.active, r.time_updated
 FROM parcel,party,relationship r left join relationship_geometry rg on r.geom_id = rg.id, spatial_source s, tenure_type t, project
 WHERE r.party_id = party.id
@@ -21,16 +21,16 @@ AND r.active = true;
 -- Show latest parcel, party, & relationship activity
 CREATE OR replace view show_activity AS
 SELECT * FROM
-(SELECT 'parcel' AS activity_type, parcel.id, s.type, NULL AS name,NULL AS parcel_id, parcel.time_created, parcel.project_id
+(SELECT 'parcel' AS activity_type, parcel.id, s.type, NULL AS name,NULL AS parcel_id, parcel.time_created, parcel.project_id, project.ckan_id
 FROM parcel, spatial_source s, project
 WHERE parcel.spatial_source = s.id
 AND parcel.project_id = project.id
 UNION all
-SELECT 'party', party.id, NULL, first_name || ' ' || lASt_name, NULL, party.time_created, party.project_id
+SELECT 'party', party.id, NULL, first_name || ' ' || lASt_name, NULL, party.time_created, party.project_id, project.ckan_id
 FROM party, project
 WHERE party.project_id = project.id
 UNION all
-SELECT 'relationship', r.id, t.type, p.first_name || ' ' || p.lASt_name AS owners, par.id::text AS parcel_id, r.time_created, r.project_id
+SELECT 'relationship', r.id, t.type, p.first_name || ' ' || p.lASt_name AS owners, par.id::text AS parcel_id, r.time_created, r.project_id, pro.ckan_id
 FROM relationship r, tenure_type t, party p, parcel par, project pro
 WHERE r.party_id = p.id
 AND r.parcel_id = par.id
@@ -41,7 +41,7 @@ Order BY time_created DESC;
 
 -- Parcel list with relationship count
 CREATE OR REPLACE VIEW show_parcels_list AS
-SELECT p.id, pro.id AS project_id, p.time_created, p.area, p.length, array_agg(t.type) as tenure_type, count(r.id) as num_relationships, p.active
+SELECT p.id, pro.id AS project_id, pro.ckan_id, p.time_created, p.area, p.length, array_agg(t.type) as tenure_type, count(r.id) as num_relationships, p.active
 FROM parcel p, relationship r, tenure_type t, project pro
 WHERE r.parcel_id = p.id
 AND p.project_id = pro.id
@@ -53,13 +53,15 @@ GROUP BY p.id, pro.id;
 
 -- Relationship History View
 CREATE OR replace view show_relationship_history AS
-SELECT -- relationship history columns
+SELECT 
+project.id as project_id, project.ckan_id,
+-- relationship history columns
 rh.relationship_id, rh.origin_id, rh.version, rh.parent_id, COALESCE(rg.geom,parcel.geom) as geom,
 parcel.id AS parcel_id,
 rh.expiration_date, rh.description, rh.date_modified, rh.active, rh.time_created,
 rh.time_updated, rh.created_by, rh.updated_by,
 -- relationship table columns
-t.type AS relationship_type, project.title as project_title,
+t.type AS relationship_type,
 s.type AS spatial_source, party.id AS party_id, first_name, last_name
 FROM parcel,party,relationship r left join relationship_geometry rg on r.geom_id = rg.id, spatial_source s, tenure_type t, relationship_history rh, project
 WHERE r.party_id = party.id
@@ -72,26 +74,31 @@ AND r.active = true;
 
 -- Parcel Resource Views
 CREATE VIEW show_parcel_resources AS
-SELECT rp.parcel_id, rp.resource_id, r.type, r.url, r.description, r.active, r.sys_delete, r.time_created, r.time_updated, r.created_by, r.updated_by, r.project_id
-from resource r, parcel p, resource_parcel rp
+SELECT r.project_id, pro.ckan_id, rp.parcel_id, rp.resource_id, r.type, r.url, r.description, r.active, r.sys_delete, r.time_created, r.time_updated, r.created_by, r.updated_by
+from resource r, parcel p, resource_parcel rp, project pro
 where rp.parcel_id = p.id
-and rp.resource_id = r.id;
+and rp.resource_id = r.id
+and r.project_id = pro.id;
+
 
 -- Party Resource Views
 CREATE VIEW show_party_resources AS
-SELECT rp.party_id, rp.resource_id, r.type, r.url, r.description, r.active, r.sys_delete, r.time_created, r.time_updated, r.created_by, r.updated_by, r.project_id
-from resource r, party p, resource_party rp
+SELECT r.project_id, pro.ckan_id, rp.party_id, rp.resource_id, r.type, r.url, r.description, r.active, r.sys_delete, r.time_created, r.time_updated, r.created_by, r.updated_by
+from resource r, party p, resource_party rp, project pro
 where rp.party_id = p.id
-and rp.resource_id = r.id;
+and rp.resource_id = r.id
+and r.project_id = pro.id;
+
 
 -- Relationship Resource Views
 CREATE VIEW show_relationship_resources AS
-SELECT rr.relationship_id, rr.resource_id, r.type, r.url, r.description, r.active, r.sys_delete, r.time_created, r.time_updated, r.created_by, r.updated_by, r.project_id
-from resource r, relationship rel, resource_relationship rr
+SELECT r.project_id, pro.ckan_id, rr.relationship_id, rr.resource_id, r.type, r.url, r.description, r.active, r.sys_delete, r.time_created, r.time_updated, r.created_by, r.updated_by
+from resource r, relationship rel, resource_relationship rr, project pro
 where rr.relationship_id = rel.id
-and rr.resource_id = r.id;
+and rr.resource_id = r.id
+and r.project_id = pro.id;
 
 -- Project Extents
 CREATE VIEW show_project_extents AS
-SELECT p.id, p.organization_id, p.title, pe.geom, p.ckan_id, p.active, p.sys_delete, p.time_created, p.time_updated, p.created_by, p.updated_by
+SELECT p.id, p.ckan_id, p.organization_id, p.title, pe.geom, p.ckan_id, p.active, p.sys_delete, p.time_created, p.time_updated, p.created_by, p.updated_by
 FROM project_extents pe right join project p on pe.project_id = p.id;
