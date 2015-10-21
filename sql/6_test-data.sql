87cd03486233d12adba96273ce5602913d441fec
Rich Gwozdz <rgwozdz@spatialdev.com>
2015-10-21 12:07:46 -0700

    Update test data script

--- sql/6_test-data.sql
+++ sql/6_test-data.sql
@@ -2,12 +2,11 @@
 
 SELECT * FROM cd_create_organization('demo-organization','demo-organization', 'Demo Organization', null);
 
-SELECT * FROM cd_create_project((Select id from organization where ckan_name = 'demo-organization'),'demo-project ' ,'demo-project ','Friday Night');
-SELECT * FROM cd_create_project((Select id from organization where ckan_id = 'demo-organization'),'nick-project','nick-project ','Nick Project');
+SELECT * FROM cd_create_project((Select id from organization where ckan_name = 'demo-organization'),'demo-project ' ,'demo-project ','Friday Night', null);
+SELECT * FROM cd_create_project((Select id from organization where ckan_id = 'demo-organization'),'nick-project','nick-project ','Nick Project', null);
 
-SELECT * FROM cd_create_organization('habit','faleCKanidHfH','Habitat for Humanity',null);
-SELECT * FROM cd_create_project((Select id from organization where ckan_id = 'habit'),'la-paz','La Paz');
-
+SELECT * FROM cd_create_organization('habitat','habitat','Habitat for Humanity',null);
+SELECT * FROM cd_create_project((Select id from organization where ckan_id = 'habitat'),'la-paz', 'la-paz', 'La Paz', 'test');
 -- ADD resources
 --SELECT * FROM cd_create_resource((select id FROM project WHERE ckan_id LIKE '%demo_project%'),'parcel',1,'http://www.cadasta.org/1/parcel',null, 'test-file.rtf');
 --SELECT * FROM cd_create_resource((select id FROM project WHERE ckan_id LIKE '%demo_project%'),'party',1,'http://www.cadasta.org/1/party',null, 'test-file.rtf');
