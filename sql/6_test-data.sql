01bb878c9bd9424346dfe360d412ac8df34ab204
Daniel Baah <dbaah@spatialdev.com>
2015-10-28 18:40:55 -0700

    remove additional test data

--- sql/6_test-data.sql
+++ sql/6_test-data.sql
@@ -84,13 +84,3 @@ SELECT * FROM cd_create_project((Select id from organization where ckan_id = 'ha
           ]
         ]
       }$anystr$);
-
--- Add additional test data
-
-SELECT * FROM cd_create_relationship(3, 1, null, 1, null, 'lease', '10/23/2009', 'Passed Down', '3rd Owner'); 
-SELECT * FROM cd_create_relationship(3, 1, null, 2, null, 'lease', '10/23/2012', 'Borrowed', 'my first apartment'); 
-SELECT * FROM cd_create_relationship(3, 1, null, 3, null, 'own', '10/23/2003', 'Inherited', 'uncle passed down'); 
-SELECT * FROM cd_create_relationship(3, 1, null, 4, null, 'lease', '10/23/1933', 'Borrowed', 'borrowing from family friend'); 
-SELECT * FROM cd_create_relationship(3, 3, null, 5, null, 'informal occupy', '10/23/2003', 'Inherited', 'uncle passed down'); 
-SELECT * FROM cd_create_relationship(3, 3, null, 6, null, 'occupy', '5/23/2002', 'Inherited', 'sister passed down'); 
-SELECT * FROM cd_create_relationship(3, 4, null, 7, null, 'occupy', '1/23/2001', 'Inherited', 'father passed down'); 
