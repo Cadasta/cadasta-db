0e50c26bfbae5d37b30fc16c45579ff7489e754f
Daniel Baah <dbaah@spatialdev.com>
2015-11-23 13:13:37 -0800

    update processdata trigger with latest change script

--- sql/3_db-functions.sql
+++ sql/3_db-functions.sql
@@ -686,7 +686,7 @@ BEGIN
                   RAISE NOTICE 'GEOLOCATION VALUE %: ', data_geom;
 
 		          -- Create new parce
-                  SELECT INTO data_parcel_id * FROM cd_create_parcel(data_project_id,'survey_sketch',data_geojson,null,null,'new description');
+                  SELECT INTO data_parcel_id * FROM cd_create_parcel(data_project_id,'survey coordinates',data_geojson,null,null,'new description');
 
                   IF data_parcel_id IS NOT NULL THEN
                     RAISE NOTICE 'New parcel id: %', data_parcel_id;
@@ -729,7 +729,6 @@ BEGIN
   END LOOP;
   RETURN NEW;
 END;
-$cd_process_data$ LANGUAGE plpgsql;
 
 CREATE TRIGGER cd_process_data AFTER INSERT ON raw_data
     FOR EACH ROW EXECUTE PROCEDURE cd_process_data();
