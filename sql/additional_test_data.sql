9c150da0045e5e7fec49a4b2352a0839954b1bb8
Daniel Baah <dbaah@spatialdev.com>
2015-11-30 10:40:00 -0800

    remove extra files

--- sql/additional_test_data.sql
+++
@@ -1,7 +0,0 @@
-﻿SELECT * FROM cd_create_relationship(1, 1, null, 1, null, 'lease', '10/23/2009', 'Passed Down', '3rd Owner'); 
-SELECT * FROM cd_create_relationship(1, 1, null, 2, null, 'lease', '10/23/2012', 'Borrowed', 'my first apartment'); 
-SELECT * FROM cd_create_relationship(1, 1, null, 3, null, 'own', '10/23/2003', 'Inherited', 'uncle passed down'); 
-SELECT * FROM cd_create_relationship(1, 1, null, 4, null, 'lease', '10/23/1933', 'Borrowed', 'borrowing from family friend'); 
-SELECT * FROM cd_create_relationship(1, 3, null, 5, null, 'informal occupy', '10/23/2003', 'Inherited', 'uncle passed down'); 
-SELECT * FROM cd_create_relationship(1, 3, null, 6, null, 'occupy', '5/23/2002', 'Inherited', 'sister passed down'); 
-SELECT * FROM cd_create_relationship(1, 4, null, 7, null, 'occupy', '1/23/2001', 'Inherited', 'father passed down');
